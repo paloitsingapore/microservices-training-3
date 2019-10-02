758105487e403aa39445de052ba4a8ad22317841
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-10-02 10:35:39 +0800

    Added the hystrix circuit breaker implementation

--- edge-service/src/main/java/com/ntuc/income/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/ntuc/income/edgeservice/EdgeServiceApplication.java
@@ -3,15 +3,26 @@ package com.ntuc.income.edgeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@Configuration
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
 	}
 
+	@Bean
+	FallBackResponse fallBackResponse(){
+		return new FallBackResponse();
+	}
 }
